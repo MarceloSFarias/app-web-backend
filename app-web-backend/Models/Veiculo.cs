e2d190de1a95888044adc57854f3d03932dfaf78
nekiol <https://github.com/MarceloSFarias/Toca-de-Oleo-Vale.git>
2022-05-21 21:48:34 -0300

    est: Adiciona o modelo veiculo.

---
+++ app-web-backend/Models/Veiculo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_web_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório informar nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar placa!")]
+        public string Placa { get; set; }
+    }
+}
